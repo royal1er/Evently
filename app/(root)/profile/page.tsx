bef28941af97739e92ea1d6418809e70ebb776db
royal1er <florianallebe6@gmail.com>
2024-01-04 14:09:07 -0400

    Page order

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -25,10 +25,10 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
       {/* My Tickets */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
+          <h3 className='h3-bold text-center sm:text-left'>Mes billets</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/#events">
-              Explore More Events
+            Découvrez d'autres événements
             </Link>
           </Button>
         </div>
@@ -37,8 +37,8 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
       <section className="wrapper my-8">
         <Collection 
           data={orderedEvents}
-          emptyTitle="No event tickets purchased yet"
-          emptyStateSubtext="No worries - plenty of exciting events to explore!"
+          emptyTitle="Aucun billet d'événement n'a encore été acheté"
+          emptyStateSubtext="Pas de panique, il y a plein d'événements passionnants à découvrir !"
           collectionType="My_Tickets"
           limit={3}
           page={ordersPage}
@@ -50,10 +50,10 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
       {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
+          <h3 className='h3-bold text-center sm:text-left'>Événements organisés</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/events/create">
-              Create New Event
+            Créer un nouvel événement
             </Link>
           </Button>
         </div>
